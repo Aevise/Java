bd0bd454e4ef0c9df23b6b3e424be5131b727b44
Aevise <111449412+Aevise@users.noreply.github.com>
2023-08-23 12:15:28 +0200

    Add files via upload

--- Typy_Generyczne/src/zad3/Zad3.java
+++ Typy_Generyczne/src/zad3/Zad3.java
@@ -4,8 +4,7 @@ package zad3;
 //charakterystycznego, np. liczby są nieparzyste, podane wyrazy są palindromami. Wykorzystaj
 //predykat.
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class Zad3 {
@@ -22,6 +21,21 @@ public class Zad3 {
         System.out.println(numberOfEvenNumbers(example));
         System.out.println(numberOfPalindromes(test));
         System.out.println(numberOfPalindromes(example));
+
+        Set<Integer> set = new HashSet<>(Arrays.asList(1,2,3,4,5,6,7,8,8,7,7,10,12,14,16));
+        Set<String> set2 = new HashSet<>(Arrays.asList("sas","sees","sd", "Go deliver a dare vile dog", "Taco cat"));
+        System.out.println("Funkcja: " + numberOfOccurences(set2, myWord -> {
+            myWord = myWord.trim().replaceAll(" ", "").toLowerCase();
+            int length = myWord.length();
+            for (int i = 0; i < length/2; i++) {
+                if(myWord.charAt(i) != myWord.charAt(length - 1 - i)){
+                    return false;
+                }
+            }
+            return true;
+        }
+        ));
+        System.out.println("Funkcja: " + numberOfOccurences(set, number->number%2 ==0));
     }
     public static<T extends Number> int numberOfEvenNumbers(List<T> numbers){
         Predicate<Number> isEven = number -> number.intValue() % 2 == 0;
@@ -42,5 +56,14 @@ public class Zad3 {
                 .stream()
                 .map(Object::toString)
                 .filter(isPalindrome).count();
+    }
+    public static <T> int numberOfOccurences(Collection<T> collection, Predicate<T> predicate){
+//        int counter = 0;
+//        for (T element : collection) {
+//            if(predicate.test(element)){
+//                counter++;
+//            }
+//        }
+        return (int)collection.stream().filter(predicate).count();
     }
 }
--- Typy_Generyczne/src/zad4/Zad4.java
+++ Typy_Generyczne/src/zad4/Zad4.java
@@ -21,7 +21,18 @@ public class Zad4 {
         System.out.println(tester);
         test.swapElements(tester, 1,3);
         System.out.println(tester);
-        test.swapElements(tester, -23, 4326);
+//        test.swapElements(tester, -23, 4326);
+
+        Integer[] tester2 = new Integer[]{1,2,3,4,5,6};
+        for (Integer integer : tester2) {
+            System.out.print(integer + " ");
+        }
+        test.swapElements(tester2, 0, 3);
+        for (Integer integer : tester2) {
+            System.out.print(integer + " ");
+        }
+
+
     }
 
     public <T> void swapElements(List<T> array, int position1, int position2){
@@ -32,5 +43,15 @@ public class Zad4 {
         T storage = array.get(position2);
         array.set(position2, array.get(position1));
         array.set(position1, storage);
+    }
+
+    public <T> void swapElements(T[] array, int position1, int position2){
+        Predicate<Integer> isOutOfBoundary = position -> position < 0 || position >= array.length;
+        if(isOutOfBoundary.test(position1) || isOutOfBoundary.test(position2)){
+            throw new RuntimeException(" Out of boundary");
+        }
+        T storage = array[position2];
+        array[position2] = array[position1];
+        array[position1] =  storage;
     }
 }
