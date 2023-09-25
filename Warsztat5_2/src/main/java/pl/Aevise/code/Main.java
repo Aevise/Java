98432e35606ce77888289f7c0979575018ad055a
Aevise <111449412+Aevise@users.noreply.github.com>
2023-09-25 20:36:45 +0200

    Add files via upload

---
+++ Warsztat5_2/src/main/java/pl/Aevise/code/Main.java
@@ -0,0 +1,8 @@
+package pl.Aevise.code;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("GRAAAAAADLE");
+    }
+}
