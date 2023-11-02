001a3865cf75cda9fe9a753623028988a0afe224
Aevise <111449412+Aevise@users.noreply.github.com>
2023-11-02 14:36:34 +0100

    Add files via upload

---
+++ Warsztat16/src/main/java/pl/Aevise/Main.java
@@ -0,0 +1,4 @@
+package pl.Aevise;
+
+public class Main {
+}
---
+++ Warsztat16/src/test/java/JpaExampleTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class JpaExampleTest {
+
+    @Test
+    void createAndUpdatePersonTest(){
+    }
+}
