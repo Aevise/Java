328dd6523bc57ed1c5e74ee8f74642651f1d4f98
Aevise <111449412+Aevise@users.noreply.github.com>
2023-12-18 14:31:21 +0100

    Add files via upload

---
+++ spring-web-spring-boot-example/src/main/java/pl/Aevise/springwebspringbootexample/SpringWebSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.Aevise.springwebspringbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebSpringBootExampleApplication.class, args);
+	}
+
+}
---
+++ spring-web-spring-boot-example/src/test/java/pl/Aevise/springwebspringbootexample/SpringWebSpringBootExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.Aevise.springwebspringbootexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSpringBootExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
