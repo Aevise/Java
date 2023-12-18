a188a09cd932aed2757ad3917697b00f90d89229
Aevise <111449412+Aevise@users.noreply.github.com>
2023-12-18 14:32:33 +0100

    Add files via upload

---
+++ warsztat_20_spring web mvc thymeleaf/src/main/java/pl/zajavka/springwebmvc/EmployeesExampleApplication.java
@@ -0,0 +1,8 @@
+package pl.zajavka;
+
+@SpringBootApplication
+public class EmployeesExampleApplication {
+    public static void main(SpringApplication.run(EmployeesExampleApplication.class, String[] args)) {
+
+    }
+}
